0b0610f082e87731ab243c6b7ca0fd43e220943d
Javier Matadamas <123751046+JM-MATADAMAS@users.noreply.github.com>
2024-04-08 09:42:41 -0600

    Se agrega los controladores

---
+++ src/controllers/authController.js
@@ -0,0 +1,76 @@
+const bcrypt = require ('bcrypt')
+const jsonwebtoken = require ('jsonwebtoken')
+const { createUser, findUserByEmail } = require('../services/user.service')
+
+exports.signup = async (req,res) => {
+  try{
+    //Código para registrarse
+    const {email, password} = request.body
+    const existingUser = await findUserByEmail(email)
+    if (existingUser.success) {
+      return res.status(400).json({
+        message: 'El usuario ya está registrado'
+      })
+    }
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password,saltRounds)
+
+    const newUser = {
+      email: email,
+      password: hashedPassword
+      //Agregar otros campos
+    }
+
+    const userResult = await createUser(newUser)
+    if (userResult.success) {
+      res.status (201).json({
+        message: 'Usuario registrado satisfactoriamente'
+      })
+    } else {
+      res.status (500).json({
+        message: 'Error al registrar el usuario'
+      })
+    }
+  }
+  catch (error){
+    res.status(500).json({
+      message: error.message
+    })
+  }
+}
+
+exports.login = async (req,res) => {
+  try{
+    //Código para logearse
+    const {email, password} = request.body
+    const findEmail = await findUserByEmail(email)
+    if (!findEmail.success){
+      res.status (401).json({
+        message: 'Usuario no encontrado'
+      })
+    }
+    const user = findEmail.user
+    const findPassword = await bcrypt.compare(password, user.password)
+    if (!findPassword.success){
+      res.status (401).json({
+        message: 'Contraseña incorrecta'
+      })
+    }
+
+    const token = jsonwebtoken.sign({
+      email: user.email,
+      userId: user.id
+    }, process.env.TOP_SECRET, {
+      expiresIn: '1h'
+    })
+
+    res.status (200).json({
+      token: token
+    })
+  }
+  catch (error){
+    res.status(500).json({
+      message: error.message
+    })
+  }
+}
